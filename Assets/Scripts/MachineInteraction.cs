fd00f955a06253b0cee1057593cfa79b651aacf0
Thomas Fynn Douglas Moorhouse <tfdm1g15@ecs.soton.ac.uk>
2017-02-11 14:05:21 +0000

    Changed protection level on ProcessingComplete Method

--- Assets/Scripts/MachineInteraction.cs
+++ Assets/Scripts/MachineInteraction.cs
@@ -4,13 +4,46 @@ using UnityEngine;
 
 public class MachineInteraction : MonoBehaviour {
 
-	// Use this for initialization
+    private GameColor color;
+    private GameObject currentItem;
+    private CharacterItemInteraction currentPlayer;
+    private int pushCount;
+    private const int COUNT_LIMIT = 20;
+
+    // Use this for initialization
 	void Start () {
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        if (currentItem != null) {
+            if (Input.GetButtonDown("Interact_" + currentPlayer.playerNo)) {
+                pushCount++;
+                if (pushCount >= COUNT_LIMIT) {
+                    complete();
+                }
+            }
+        }
 	}
+
+    bool Interact (CharacterItemInteraction playerCII, GameObject obj) {
+        if (playerCII.color == color) {
+            if (obj != null) {
+                currentItem = obj;
+                currentPlayer = playerCII;
+                pushCount = 0;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private void complete() {
+        //add color
+        currentPlayer.ProcessingComplete(currentItem);
+        currentPlayer = null;
+        currentItem = null;
+    }
+
 }
