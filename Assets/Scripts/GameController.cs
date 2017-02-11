7ded03bb7a148fd829aa3eda3b017f3c0ea5c1d6
Thomas Fynn Douglas Moorhouse <tfdm1g15@ecs.soton.ac.uk>
2017-02-11 14:53:14 +0000

    Created Game controller script skeleton

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+    private int score;
+    private float timeRemaining;
+
+
+	// Use this for initialization
+	void Start () {
+        score = 0;
+        timeRemaining = 90f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timeRemaining -= Time.deltaTime;
+        if (timeRemaining <= 0) {
+            endGame();
+        }
+	}
+
+    public void addPoint() {
+        score += 1;
+    }
+
+
+    void endGame() {
+
+    }
+
+}
