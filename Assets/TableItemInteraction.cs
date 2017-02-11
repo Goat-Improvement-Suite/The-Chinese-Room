dde518448daada1695f823de17b8ed955a15152b
fierydrake <m.j.tunnicliffe@gmail.com>
2017-02-11 23:34:18 +0000

    Add tableiteminteraction to table slots

---
+++ Assets/TableItemInteraction.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TableItemInteraction : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
