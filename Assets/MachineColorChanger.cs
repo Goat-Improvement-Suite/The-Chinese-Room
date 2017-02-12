0c9beef6e346abc85ecba3f8f20b66c99e84f33d
fierydrake <m.j.tunnicliffe@gmail.com>
2017-02-12 15:13:09 +0000

    Make machines change color

---
+++ Assets/MachineColorChanger.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MachineColorChanger : MonoBehaviour {
+    public SpriteRenderer[] machineBacks;
+    public SpriteRenderer[] machineFrames;
+    public ConvayerInItemInteraction inputConveyer;
+    public ConvayerOutItemInteraction outputConveyer;
+
+    public Sprite redMachineBack;
+    public Sprite blueMachineBack;
+    public Sprite greenMachineBack;
+    public Sprite yellowMachineBack;
+
+    public Sprite redMachineFrame;
+    public Sprite blueMachineFrame;
+    public Sprite greenMachineFrame;
+    public Sprite yellowMachineFrame;
+
+    public void ChangeColor(GameColor newColor) {
+        GetComponent<MachineItemInteraction>().color = newColor;
+        foreach (var machineBack in machineBacks) {
+            machineBack.sprite = machineBackForColor(newColor);
+        }
+        foreach (var machineFrame in machineFrames) {
+            machineFrame.sprite = machineFrameForColor(newColor);
+        }
+        inputConveyer.color = newColor;
+        outputConveyer.color = newColor;
+    }
+
+    public Sprite machineBackForColor(GameColor color) {
+        switch (color) {
+            case GameColor.Red: return redMachineBack;
+            case GameColor.Blue: return blueMachineBack;
+            case GameColor.Green: return greenMachineBack;
+            case GameColor.Yellow: return yellowMachineBack;
+        }
+        return blueMachineBack;
+    }
+
+    public Sprite machineFrameForColor(GameColor color) {
+        switch (color) {
+            case GameColor.Red: return redMachineFrame;
+            case GameColor.Blue: return blueMachineFrame;
+            case GameColor.Green: return greenMachineFrame;
+            case GameColor.Yellow: return yellowMachineFrame;
+        }
+        return blueMachineBack;
+    }
+}
--- Assets/Scripts/ColorManager.cs
+++ Assets/Scripts/ColorManager.cs
@@ -8,6 +8,11 @@ public class ColorManager : MonoBehaviour {
     public GameObject wall;
     private SpriteRenderer wallRenderer;
 
+    public MachineColorChanger topRightMachine;
+    public MachineColorChanger topLeftMachine;
+    public MachineColorChanger bottomLeftMachine;
+    public MachineColorChanger bottomRightMachine;
+
     public Sprite[] walls;
     public Sprite[] floors;
     public ColorManager setCurrent(int x)
@@ -18,6 +23,32 @@ public class ColorManager : MonoBehaviour {
         {
             floorRenderer.sprite = floors[current % floors.Length];
             wallRenderer.sprite = walls[current % walls.Length];
+            switch (current % walls.Length) {
+                case 0:
+                    topRightMachine.ChangeColor(GameColor.Blue);
+                    topLeftMachine.ChangeColor(GameColor.Red);
+                    bottomLeftMachine.ChangeColor(GameColor.Yellow);
+                    bottomRightMachine.ChangeColor(GameColor.Green);
+                    break;
+                case 1:
+                    topRightMachine.ChangeColor(GameColor.Red);
+                    topLeftMachine.ChangeColor(GameColor.Yellow);
+                    bottomLeftMachine.ChangeColor(GameColor.Green);
+                    bottomRightMachine.ChangeColor(GameColor.Blue);
+                    break;
+                case 2:
+                    topRightMachine.ChangeColor(GameColor.Yellow);
+                    topLeftMachine.ChangeColor(GameColor.Green);
+                    bottomLeftMachine.ChangeColor(GameColor.Blue);
+                    bottomRightMachine.ChangeColor(GameColor.Red);
+                    break;
+                case 3:
+                    topRightMachine.ChangeColor(GameColor.Green);
+                    topLeftMachine.ChangeColor(GameColor.Blue);
+                    bottomLeftMachine.ChangeColor(GameColor.Red);
+                    bottomRightMachine.ChangeColor(GameColor.Yellow);
+                    break;
+            }
         }
         return this;
     }
