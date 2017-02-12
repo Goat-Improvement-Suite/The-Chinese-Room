4c22ba6280731483cb23cf8e8cdf14ad25d3548c
fierydrake <m.j.tunnicliffe@gmail.com>
2017-02-12 12:06:18 +0000

    Add goal glow effect (not hooked up yet)

---
+++ Assets/Glow.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Glow : MonoBehaviour {
+    public SpriteRenderer spriteRenderer;
+    public float minScale;
+    public float maxScale;
+    public Color startColor;
+    public Color endColor;
+
+    public bool glowing;
+    public float pulseSpeed;
+    private float glowStartTime;
+    private bool wasGlowing = false;
+	
+	void Start () {		
+	}
+	
+	void Update () {
+	    if (glowing) {
+            if (!wasGlowing) {
+                spriteRenderer.enabled = true;
+                glowStartTime = Time.time;
+            }
+
+            float t = Mathf.Sin(pulseSpeed * (Time.time - glowStartTime));
+
+            // Color
+            spriteRenderer.color = Color.Lerp(startColor, endColor, t);
+
+            // Scale
+            float range = Mathf.Clamp(maxScale - minScale, 0, maxScale) / 2f;
+            float newScale = (minScale + maxScale) / 2f + range * t;
+            transform.localScale = new Vector3(newScale, newScale, 1);
+        } else {
+            if (wasGlowing) {
+                spriteRenderer.enabled = false;
+            }
+        }
+        wasGlowing = glowing;
+	}
+}
