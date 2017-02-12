4d6b2d82ef8dad395eab17790c7ec6315c5d7e47
Thomas Fynn Douglas Moorhouse <tfdm1g15@ecs.soton.ac.uk>
2017-02-12 02:05:20 +0000

    Sperated Audio Controllers and added panics and fades

--- Assets/Scripts/MainMenuController.cs
+++ Assets/Scripts/MainMenuController.cs
@@ -2,21 +2,38 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenuController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    private bool fadingOut;
+    private CanvasScaler scaler;
+    private AudioController audioController;
+
+    // Use this for initialization
+    void Start () {
+        fadingOut = false;
+        scaler = GameObject.Find("Canvas").GetComponent<CanvasScaler>();
+        audioController = GameObject.Find("AudioController").GetComponent<AudioController>();
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
+        if (fadingOut) {
+            scaler.scaleFactor -= Time.deltaTime;
+            if (scaler.scaleFactor < 0.03f) {
+                scaler.scaleFactor = 0.03f;
+                SceneManager.LoadScene("chinese_room");
+            }
+        }
 	}
 
     void StartGame() {
-        SceneManager.LoadScene("chinese_room");
+        if (!fadingOut) {
+            audioController.StartFade();
+            fadingOut = true;
+            //System.Threading.Thread.Sleep(500);
+        }
     }
 
     void QuitGame() {
